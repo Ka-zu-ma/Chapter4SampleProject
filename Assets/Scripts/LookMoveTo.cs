a65bdb14f606f117918c31169dc70ce29144e1aa
Kazuma_M <km.km.kzkz00@gmail.com>
2021-03-05 19:34:25 +0900

    ユーザーが向く方向移動処理修正
    ユーザーが向く方向にEthanが動くように修正

--- Assets/Scripts/LookMoveTo.cs
+++ Assets/Scripts/LookMoveTo.cs
@@ -5,21 +5,18 @@ using UnityEngine;
 public class LookMoveTo : MonoBehaviour
 {
     public GameObject ground;
+    public Transform centerEyeAnchor;
     // Start is called before the first frame update
-    void Start(){
-    }
+    //void Start(){
+    //}
 
     // Update is called once per frame
     void Update(){
-        //元々は、Transform camera = Camera.main.transform;
-        Camera camera = GetComponent<OVRCameraRig>().leftEyeCamera;
         Ray ray;
         RaycastHit hit;
         GameObject hitObject;
 
-        Debug.DrawRay(camera.transform.position, camera.transform.rotation * Vector3.forward * 100.0f);
-
-        ray = new Ray(camera.transform.position, camera.transform.rotation * Vector3.forward);
+        ray = new Ray(centerEyeAnchor.position, centerEyeAnchor.forward);
 
         if (Physics.Raycast(ray, out hit)) {
             hitObject = hit.collider.gameObject;
