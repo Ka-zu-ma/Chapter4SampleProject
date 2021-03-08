6cadc1d3c202a03f53f37c3abcabe99f03893ee9
Kazuma_M <km.km.kzkz00@gmail.com>
2021-03-08 14:59:03 +0900

    カーソルが詰まる現象修正

--- Assets/Scripts/LookMoveTo.cs
+++ Assets/Scripts/LookMoveTo.cs
@@ -6,19 +6,18 @@ public class LookMoveTo : MonoBehaviour
 {
     public GameObject ground;
     public Transform centerEyeAnchor;
-    // Start is called before the first frame update
-    //void Start(){
-    //}
+    RaycastHit[] hits;
 
     // Update is called once per frame
     void Update(){
         Ray ray;
-        RaycastHit hit;
         GameObject hitObject;
 
         ray = new Ray(centerEyeAnchor.position, centerEyeAnchor.forward);
+        hits = Physics.RaycastAll(ray);
 
-        if (Physics.Raycast(ray, out hit)) {
+        for (int i = 0; i < hits.Length; i++) {
+            RaycastHit hit = hits[i];
             hitObject = hit.collider.gameObject;
             if (hitObject == ground) {
                 Debug.Log("HIt(x,y,z): " + hit.point.ToString("F2"));
