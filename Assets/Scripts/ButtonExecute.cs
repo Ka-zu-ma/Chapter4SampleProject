92b91959e487d91d6b9d406ff3743f15b6aa2b0c
Kazuma_M <km.km.kzkz00@gmail.com>
2021-03-12 17:16:59 +0900

    Clicker.cs作成、視線クリック選択、視線見つめて選択

--- Assets/Scripts/ButtonExecute.cs
+++ Assets/Scripts/ButtonExecute.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ButtonExecute : MonoBehaviour {
+	public float timeToSelect = 2.0f;
+	private float countDown;
 	private GameObject currentButton;
 	public Transform centerEyeAnchor;
+	private Clicker clicker = new Clicker();
 
 	// Start is called before the first frame update
 	void Start() {
@@ -28,11 +31,21 @@ public class ButtonExecute : MonoBehaviour {
 			if (currentButton != null) {//　ハイライトを外す
 				ExecuteEvents.Execute<IPointerExitHandler>(currentButton, data, ExecuteEvents.pointerExitHandler);
 			}
-		}
-		currentButton = hitButton;
 
-		if (currentButton != null) {// ハイライトする
-			ExecuteEvents.Execute<IPointerEnterHandler>(currentButton, data, ExecuteEvents.pointerEnterHandler);
+			currentButton = hitButton;
+
+			if (currentButton != null) {// ハイライトする
+				ExecuteEvents.Execute<IPointerEnterHandler>(currentButton, data, ExecuteEvents.pointerEnterHandler);
+				countDown = timeToSelect;
+			}
+		}
+
+		if (currentButton != null) {
+			countDown -= Time.deltaTime;
+			if (clicker.clicked() || countDown < 0.0f) {
+				ExecuteEvents.Execute<IPointerClickHandler>(currentButton, data, ExecuteEvents.pointerClickHandler);
+				countDown = timeToSelect;
+			}
 		}
 	}
 }
