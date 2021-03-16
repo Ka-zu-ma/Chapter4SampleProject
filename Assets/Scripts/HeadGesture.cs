b02bf12d457ff24d6bd4e6665b4ccff3a0ca0cad
Kazuma_M <km.km.kzkz00@gmail.com>
2021-03-16 16:19:58 +0900

    FlippinDashboard.cs、HeadGesture.cs追加

---
+++ Assets/Scripts/HeadGesture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadGesture : MonoBehaviour {
+	public bool isFacingDown = false;
+	public Transform centerEyeAnchor;
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		isFacingDown = DetectFacingDown();
+	}
+
+	private bool DetectFacingDown() {
+		return (CameraAngleFromGround() < 60.0f);
+	}
+
+	private float CameraAngleFromGround() {
+		return Vector3.Angle(Vector3.down, centerEyeAnchor.rotation * Vector3.forward);
+	}
+}
