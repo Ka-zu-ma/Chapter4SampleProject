9901cdd1b5c32f80308e19b235c14265717343a2
Kazuma_M <km.km.kzkz00@gmail.com>
2021-03-19 19:45:37 +0900

    視線方向への移動

---
+++ Assets/Scripts/HeadLookWalk.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadLookWalk : MonoBehaviour {
+	public Transform centerEyeAnchor;
+	public float velocity = 0.7f;
+
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		Vector3 moveDirection = centerEyeAnchor.forward;
+		moveDirection *= velocity * Time.deltaTime;
+		transform.position += moveDirection;
+	}
+}
